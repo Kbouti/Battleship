31e1c0e94490a854a49be389e72f3b619ece12af
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-01-29 17:27:20 -0800

    Create input fields for names

--- src/domElements.js
+++ src/domElements.js
@@ -1,13 +1,12 @@
 // idk if we need to import any functions yet
 
+const { create } = require("lodash");
+
 // Need to create the homePage, a form that asks for the users name and how many players
 
 function createElement(type, parentElement, id, classes) {
-  console.log(`create Element triggered`);
   const newElement = document.createElement(type);
-  console.log(`test1`);
   if (classes !== null) {
-    console.log(`test2`);
     for (let i = 0; i < classes.length; i++) {
       newElement.classList.add(classes[i]);
     }
@@ -25,11 +24,67 @@ function buildPage() {
   const header = createElement(`div`, body, "header", []);
   const title = createElement("H1", header, "title", ["title"]);
   title.innerHTML = "Battleship";
+  // ******************************************************************************************
+  // Form:
+  const newGameForm = createElement("form", body, "newGameForm", []);
+  const formHeader = createElement("div", newGameForm, "newGameFormHeader", []);
+  const formTitle = createElement("H2", formHeader, "newGameTitle", ["form"]);
+  formTitle.innerHTML = `New Game`;
+  const formContent = createElement("div", newGameForm, "formContent", []);
+  const formContentHalf1 = createElement(
+    `div`,
+    formContent,
+    "formContentHalf1",
+    ["formContentHalf"]
+  );
 
-  const newGameForm = createElement('form', body, "newGameForm", []);
-  const formTitle = createElement('H2', newGameForm, "newGameTitle", ["form"])
-  formTitle.innerHTML = `New Game`
+  const player1Label = createElement(
+    "label",
+    formContentHalf1,
+    "player1Label",
+    ["formLabel"]
+  );
+  player1Label.innerHTML = `Player 1 Name:`;
+  player1Label.setAttribute("for", "player1Input");
 
+  const player1Input = createElement(
+    "input",
+    formContentHalf1,
+    "player1Input",
+    ["formInput"]
+  );
+  player1Input.setAttribute("type", "text");
+  player1Input.setAttribute("placeholder", "What's your name?");
+
+  const formContentHalf2 = createElement(
+    `div`,
+    formContent,
+    "formContentHalf2",
+    ["formContentHalf"]
+  );
+
+  const player2Label = createElement(
+    "label",
+    formContentHalf2,
+    "player2Label",
+    ["formLabel"]
+  );
+  player2Label.innerHTML = `Player 2 Name:`;
+  player2Label.setAttribute("for", "player2Input");
+
+  const player2Input = createElement(
+    "input",
+    formContentHalf2,
+    "player2Input",
+    ["formInput"]
+  );
+  player2Input.setAttribute("type", "text");
+  player2Input.setAttribute("placeholder", "Admiral A.I.");
+
+  const formFooter = createElement(`div`, newGameForm, "formFooter", []);
+
+  // End Form
+  // ******************************************************************************************
 }
 
 // *************************************************************************************
