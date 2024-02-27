e0ee7e2c3c81022ad5c2d2c6c00f3bc53afe4cc0
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-02-27 10:10:32 -0800

    Update HTML title

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   plugins: [
     new htmlWebpackPlugin({
-        title: 'Battleship Template',
+        title: 'Battleship',
         filename: 'index.html',
         inject: 'body',
     })
